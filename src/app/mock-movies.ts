4b9abb78ffa46b290349a91fc7104f08c9cb4510
lucassanders <lucasbsanders@ufl.edu>
2019-04-23 23:58:27 -0400

    Add files via upload

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -1,8 +1,9 @@
 import { Movie } from './movie';
 
 export const MOVIES: Movie[] = [
-  { title: 'Titanic (1997)', description: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built.', poster: './movie-posters/titanic.png' },
-  { title: 'Gattaca (1997)', description: 'The film presents a biopunk vision of a future society driven by eugenics where potential children are conceived through genetic selection to ensure they possess the best hereditary traits of their parents.[5] The film centers on Vincent Freeman, played by Hawke, who was conceived outside the eugenics program and struggles to overcome genetic discrimination to realize his dream of going into space. ', poster: '../movie-posters/gattaca.png'  },
+  //{ title: 'Titanic (1997)', description: 'James Cameron\'s "Titanic" is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built.', poster: 'app/movie-posters/titanic.png' },
+
   { title: 'Spirited Away (2001)', description: 'Tells the story of Chihiro Ogino (Hiiragi), a sullen 10-year-old girl who, while moving to a new neighborhood, enters the world of Kami (spirits) of Japanese Shinto folklore', poster: '../movie-posters/spiritedaway.png'  },
+{ title: 'Gattaca (1997)', description: 'The film presents a biopunk vision of a future society driven by eugenics where potential children are conceived through genetic selection to ensure they possess the best hereditary traits of their parents.[5] The film centers on Vincent Freeman, played by Hawke, who was conceived outside the eugenics program and struggles to overcome genetic discrimination to realize his dream of going into space. ', poster: '../movie-posters/gattaca.png'  },
   { title: 'The Avengers (2012)', description: 'When Thor\'s evil brother, Loki (Tom Hiddleston), gains access to the unlimited power of the energy cube called the Tesseract, Nick Fury (Samuel L. Jackson), director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth', poster: '../movie-posters/avengers.png'  }
 ];
